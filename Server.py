694d26251e6b59fbd1e8fd54b25bc7c17c146cb6
Umarhegde <umarhegdee@gmail.com>
2020-05-26 16:20:05 +0530

    my portfolio

---
+++ Server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
+
+
+# @app.route('/blog')
+# def blog():
+#     return 'This is my thoughts on blog'
